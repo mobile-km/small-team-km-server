d3bdeb5eaa3c4b84823452872358865494a395b2
fushang318 <fushang318@gmail.com>
2012-07-09 11:42:43 +0800

    fix prev commit

--- app/controllers/api/chat_nodes_controller.rb
+++ app/controllers/api/chat_nodes_controller.rb
@@ -6,7 +6,7 @@ class Api::ChatNodesController < ApplicationController
     if chat_node.save
       render :json=>{
         :server_chat_node_id=>chat_node.id,
-        :server_created_time=>chat_node.created_at
+        :server_created_time=>chat_node.created_at.to_i
       }
     else
       render :status=>411,:text=>"411"

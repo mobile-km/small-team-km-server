388f814bdc2d2309ac202bdb040201feb09714c9
fushang318 <fushang318@gmail.com>
2012-10-24 09:55:35 +0800

    data_list_json 增加  forked_from_id

--- app/models/data_list.rb
+++ app/models/data_list.rb
@@ -28,6 +28,7 @@ class DataList < ActiveRecord::Base
       :kind       => self.kind,
       :public     => self.public?.to_s,
       :has_commits => self.has_commits?.to_s,
+      :forked_from_id => self.forked_from_id,
       :creator => {
         :id => self.creator.id,
         :name => self.creator.name,

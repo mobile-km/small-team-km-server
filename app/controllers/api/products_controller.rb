4eb81dc68b1edc26a5ddd3edd74298b650c82e02
Kaid <kaid@kaid.me>
2013-01-10 18:19:03 +0800

    rescue BadBarcodeError in controller

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,5 +1,7 @@
 class Api::ProductsController < ApplicationController
   def search
     render :json => Product.query(params[:code], params[:format])
+  rescue ProductFetcher::BadBarcodeError => e
+    render :json => {:error => e.message}
   end
 end

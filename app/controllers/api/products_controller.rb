051357c724e8c9b784a64c61ef41d693477d7fc1
Kaid <kaid@kaid.me>
2013-01-11 16:34:26 +0800

    wrapped the error rescue response into an array in ProductsController

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,6 +2,6 @@ class Api::ProductsController < ApplicationController
   def search
     render :json => Product.query(params[:code], params[:format])
   rescue ProductFetcher::BadBarcodeError => e
-    render :json => {:error => e.message}
+    render :json => [{:error => e.message}]
   end
 end

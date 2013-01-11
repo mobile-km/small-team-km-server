820e5e1e6ac08d29f08f2bf6fd3214ad4a89d40b
Kaid <kaid@kaid.me>
2013-01-11 10:07:08 +0800

    refactored ProductFetcher a bit

--- lib/product_fetcher.rb
+++ lib/product_fetcher.rb
@@ -1,33 +1,46 @@
 # -*- coding: utf-8 -*-
 class ProductFetcher
-  NAME_URL = 'http://android.wochacha.com/Qrsearch/Price'
+  NAME_URL   = 'http://android.wochacha.com/Qrsearch/Price'
   VENDOR_URL = 'http://search.anccnet.com/searchResult.aspx'
   
-  attr_reader :barcode, :format
-
   def initialize(barcode, format)
-    @code = barcode
-    @format  = format
+    @code   = barcode
+    @format = format
     build_request_urls
   end
 
   def fetch
-    @name = parse_name Net::HTTP.get(@name_request_url)
-    @vendor = parse_vendor Net::HTTP.get(@vendor_request_url)
-    @result = {:name => @name, :vendor => @vendor, :code => @code, :code_format => @format}
+    @name   = fetch_name
+    @vendor = fetch_vendor
     return self
   end
 
   def create_product
-    product = Product.create @result
-    raise BadBarcodeError.new('错误的商品条码') if product.errors.any?
+    raise BadBarcodeError.new('错误的商品条码') unless product_created?
+  end
+
+  def result
+    return unless @name
+    {:name => @name, :vendor => @vendor, :code => @code, :code_format => @format}
   end
 
 private
 
+  def product_created?
+    !Product.create(self.result).errors.any?
+  end
+
   def build_request_urls
-    @name_request_url = URI("#{NAME_URL}?barcode=#{@code}&encoding=#{@format}")
+    @name_request_url   = URI("#{NAME_URL}?barcode=#{@code}&encoding=#{@format}")
     @vendor_request_url = URI("#{VENDOR_URL}?keyword=#{@code}")
+  end
+
+  def fetch_name
+    parse_name Net::HTTP.get(@name_request_url)
+  end
+
+  def fetch_vendor
+    parse_vendor Net::HTTP.get(@vendor_request_url)
   end
 
   def parse_name(response)
